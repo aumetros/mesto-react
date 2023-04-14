54ebf8a3e38bd69af9358e015d6bba2e1829e40c
aumetros <aum3tros@yandex.ru>
2023-04-14 20:43:27 +0300

    Создали сет данных пользователя

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,23 @@
-function Main({onEditAvatar, onEditProfile, onAddPlace}) {
+import React from "react";
+
+function Main({ onEditAvatar, onEditProfile, onAddPlace }) {
+  const [userName, setUserName] = React.useState("");
+  const [userDescription, setUserDescription] = React.useState("");
+  const [userAvatar, setUserAvatar] = React.useState("");
+
   return (
     <main>
       <section className="profile">
-        <div className="profile__avatar" onClick={onEditAvatar}>
+        <div
+          className="profile__avatar"
+          style={{ backgroundImage: `url(${userAvatar})` }}
+          onClick={onEditAvatar}
+        >
           <div className="profile__avatar-overlay"></div>
         </div>
 
         <div className="profile__info">
-          <h1 className="profile__name">#</h1>
+          <h1 className="profile__name">{userName}</h1>
 
           <button
             className="profile__edit-button"
@@ -15,7 +25,7 @@ function Main({onEditAvatar, onEditProfile, onAddPlace}) {
             onClick={onEditProfile}
           ></button>
 
-          <p className="profile__about"></p>
+          <p className="profile__about">{userDescription}</p>
         </div>
 
         <button
