4ed0bb0dd496821829ad4c81c0ebadb74d69fddc
aumetros <aum3tros@yandex.ru>
2023-04-14 19:01:14 +0300

    Создан компонент ImagePopup

---
+++ src/components/ImagePopup.js
@@ -0,0 +1,14 @@
+function ImagePopup() {
+  return (
+    <section className="popup-image popup popup_type_image">
+      <div className="popup-image__container">
+        <img className="popup-image__item" src="#" alt="#" />
+        <p className="popup-image__subtitle"></p>
+
+        <button className="popup__close" type="button"></button>
+      </div>
+    </section>
+  );
+}
+
+export default ImagePopup;
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,29 +1,22 @@
-function PopupWithForm({title, name}) {
-
+function PopupWithForm({ title, name, children }) {
   return (
     <section className={`popup-${name} popup`}>
-        <div className={`popup-${name}__container`}>
-          <form
-            className="popup__form"
-            name={name}
-            noValidate
-          >
-            <h2 className="popup__title">{title}</h2>
-            
-            <button
-              className="popup__button"
-              type="submit"
-            >
-              <span className="popup__button-text">Сохранить</span>
-              <span className="popup__button-spinner">Сохранение...</span>
-            </button>
-          </form>
+      <div className={`popup-${name}__container`}>
+        <form className="popup__form" name={name} noValidate>
+          <h2 className="popup__title">{title}</h2>
 
-          <button className="popup__close" type="button"></button>
-        </div>
-      </section>
-  )
+          {children}
 
+          <button className="popup__button" type="submit">
+            <span className="popup__button-text">Сохранить</span>
+            <span className="popup__button-spinner">Сохранение...</span>
+          </button>
+        </form>
+
+        <button className="popup__close" type="button"></button>
+      </div>
+    </section>
+  );
 }
 
 export default PopupWithForm;
